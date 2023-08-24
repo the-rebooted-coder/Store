79647ad835de9ddc630c14268397582d081d6e2e
Spandan Saxena <spandansaxena@gmail.com>
2023-08-24 15:22:43 +0530

    Image can be downloaded now

--- app/src/main/java/com/onesilicondiode/store/ListAdapter.java
+++ app/src/main/java/com/onesilicondiode/store/ListAdapter.java
@@ -2,7 +2,11 @@ package com.onesilicondiode.store;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.app.DownloadManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
@@ -10,24 +14,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.bumptech.glide.Glide;
 import com.pedromassango.doubleclick.DoubleClick;
 import com.pedromassango.doubleclick.DoubleClickListener;
 
+import java.io.File;
+import android.Manifest;
 import java.util.List;
 
 public class ListAdapter extends ArrayAdapter<SecureVaultModel> {
     private final Activity mContext;
     private List<SecureVaultModel> foodList;
 
-    public ListAdapter(Activity mContext, List<SecureVaultModel> foodList){
-        super(mContext,R.layout.list_item,foodList);
+    public ListAdapter(Activity mContext, List<SecureVaultModel> foodList) {
+        super(mContext, R.layout.list_item, foodList);
         this.mContext = mContext;
         this.foodList = foodList;
     }
@@ -35,7 +44,7 @@ public class ListAdapter extends ArrayAdapter<SecureVaultModel> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        LayoutInflater inflater =  mContext.getLayoutInflater();
+        LayoutInflater inflater = mContext.getLayoutInflater();
         View listItemView = inflater.inflate(R.layout.list_item, null, true);
         ImageView foodImage = listItemView.findViewById(R.id.imageLoader);
         SecureVaultModel food = foodList.get(position);
@@ -65,7 +74,6 @@ public class ListAdapter extends ArrayAdapter<SecureVaultModel> {
         Glide.with(getContext())
                 .load(url)
                 .override(500, 500)
-         //       .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .fitCenter()
                 .into(foodImage);
         return listItemView;
@@ -87,18 +95,53 @@ public class ListAdapter extends ArrayAdapter<SecureVaultModel> {
         dialog.setContentView(R.layout.full_screen_image_dialog);
 
         ImageView fullScreenImageView = dialog.findViewById(R.id.fullScreenImageView);
+        Button downloadButton = dialog.findViewById(R.id.downloadButton);
 
         Glide.with(mContext)
                 .load(imageUrl)
                 .into(fullScreenImageView);
 
-        fullScreenImageView.setOnClickListener(new View.OnClickListener() {
+        // Handle the download button click event
+        downloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialog.dismiss();
+                // Check for permission to write to external storage
+                if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    // Permission is not granted, request it from the user
+                    ActivityCompat.requestPermissions(mContext, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE);
+                } else {
+                    // Permission is granted, proceed with download
+                    initiateDownload(imageUrl);
+                    // Dismiss the dialog after initiating the download
+                    dialog.dismiss();
+                }
             }
         });
 
         dialog.show();
+    }
+
+    // Request code for write external storage permission
+    private static final int PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
+
+    // Method to initiate the download after permission is granted
+    private void initiateDownload(String imageUrl) {
+        DownloadManager downloadManager = (DownloadManager) mContext.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(imageUrl);
+
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setTitle("Image Download");
+        request.setDescription("Downloading image...");
+
+        // Set the destination directory for the downloaded image
+        File destinationDirectory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "Vault");
+        destinationDirectory.mkdirs();
+        request.setDestinationUri(Uri.fromFile(new File(destinationDirectory, "vault-storage.jpg")));
+
+        downloadManager.enqueue(request);
+
+        // Show a toast to inform the user about the download
+        Toast.makeText(mContext, "Download started...", Toast.LENGTH_SHORT).show();
     }
 }
