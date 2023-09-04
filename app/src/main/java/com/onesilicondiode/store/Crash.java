62e7391775a7127ae6e64ee0e1ff5f38daed0599
Spandan Saxena <spandansaxena@gmail.com>
2023-09-04 18:13:20 +0530

    Added Full Biometric to the Crash

--- app/src/main/java/com/onesilicondiode/store/Crash.java
+++ app/src/main/java/com/onesilicondiode/store/Crash.java
@@ -1,15 +1,22 @@
 package com.onesilicondiode.store;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
 public class Crash extends AppCompatActivity {
+    private MaterialButton showFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        showBottomSheet();
+        setContentView(R.layout.activity_crash);
+        showFrag = findViewById(R.id.showFrag);
+        showFrag.setOnClickListener(view -> showBottomSheet());
+
     }
 
     private void showBottomSheet() {
