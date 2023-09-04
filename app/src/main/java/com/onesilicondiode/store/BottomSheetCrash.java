85a62d55a03f7708602dfa036ba075190bedd057
Spandan Saxena <spandansaxena@gmail.com>
2023-09-04 17:59:19 +0530

    Biometric Auth Added

--- app/src/main/java/com/onesilicondiode/store/BottomSheetCrash.java
+++ app/src/main/java/com/onesilicondiode/store/BottomSheetCrash.java
@@ -1,7 +1,13 @@
 package com.onesilicondiode.store;
 
+import static android.content.Context.VIBRATOR_SERVICE;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,19 +16,53 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.biometric.BiometricPrompt;
+
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class BottomSheetCrash extends BottomSheetDialogFragment {
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
+    private Vibrator vibrator;
+    private MaterialButton useBiometric;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        executor = Executors.newSingleThreadExecutor();
+        biometricPrompt = new BiometricPrompt(this, executor, new BiometricCallback());
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Unlock Store")
+                .setSubtitle("Use your fingerprint")
+                .setNegativeButtonText("Go Back")
+                .build();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate your bottom sheet layout here
         View view = inflater.inflate(R.layout.bottom_sheet_layout, container, false);
         TextInputLayout inputLayoutPassword = view.findViewById(R.id.inputLayoutPassword);
         TextInputEditText inputPassword = view.findViewById(R.id.inputPassword);
+        useBiometric = view.findViewById(R.id.useBiometric);
+        if (getActivity() != null) {
+            vibrator = (Vibrator) getActivity().getSystemService(VIBRATOR_SERVICE);
+        }
         Button unlockButton = view.findViewById(R.id.unlockApp);
         Animation shakeAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.shake);
+        useBiometric.setOnClickListener(v -> {
+            vibrate();
+            biometricPrompt.authenticate(promptInfo);
+        });
         unlockButton.setOnClickListener(v -> {
             // Get the entered PIN
             String enteredPin = inputPassword.getText().toString();
@@ -32,6 +72,7 @@ public class BottomSheetCrash extends BottomSheetDialogFragment {
                 // PIN is correct, navigate to Landing Activity
                 Intent intent = new Intent(getActivity(), Landing.class);
                 startActivity(intent);
+                vibrate();
                 if (getActivity() != null) {
                     getActivity().finish();
                 }
@@ -45,5 +86,25 @@ public class BottomSheetCrash extends BottomSheetDialogFragment {
         });
 
         return view;
+    }
+
+    private class BiometricCallback extends BiometricPrompt.AuthenticationCallback {
+        @Override
+        public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+            super.onAuthenticationSucceeded(result);
+            // Authentication succeeded, start the Landing Activity
+            Intent intent = new Intent(getActivity(), Landing.class);
+            startActivity(intent);
+            vibrate();
+            if (getActivity() != null) {
+                getActivity().finish();
+            }
+            dismiss();
+        }
+    }
+    private void vibrate() {
+        long[] pattern = {23,0,17,4,15,11,19,15,18,13,16,8,20,2,0,0,14,0,14,5,0,17,16};
+        VibrationEffect vibrationEffect = VibrationEffect.createWaveform(pattern, -1);
+        vibrator.vibrate(vibrationEffect);
     }
 }
--- app/src/main/java/com/onesilicondiode/store/Crash.java
+++ app/src/main/java/com/onesilicondiode/store/Crash.java
@@ -1,162 +1,19 @@
 package com.onesilicondiode.store;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.security.keystore.KeyGenParameterSpec;
-import android.security.keystore.KeyProperties;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.hardware.fingerprint.FingerprintManagerCompat;
-
-import java.security.KeyStore;
-
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 
 public class Crash extends AppCompatActivity {
-    private FingerprintManagerCompat fingerprintManager;
-    private int titleClickCount = 0;
-    private static final String KEY_NAME = "my_key_name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         showBottomSheet();
-        fingerprintManager = FingerprintManagerCompat.from(this);
-        if (fingerprintManager.isHardwareDetected() && fingerprintManager.hasEnrolledFingerprints()) {
-            authenticateWithFingerprint();
-        } else {
-            Toast.makeText(this,"Fingerprint not available, tap the title 4 times to enter pin manually",Toast.LENGTH_LONG).show();
-        }
-        View dialogView = getLayoutInflater().inflate(R.layout.custom_dialog, null);
-        // Create an AlertDialog
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setView(dialogView);
-
-        // Initialize the titleClickCount to 0
-        titleClickCount = 0;
-
-        // Find the title TextView in the custom dialog layout
-        TextView titleTextView = dialogView.findViewById(R.id.title_text_view);
-
-        // Set a click listener on the title TextView
-        titleTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                titleClickCount++;
-
-                // Check if the title is clicked twice
-                if (titleClickCount == 2) {
-                    showPinCodeDialog();
-                }
-            }
-        });
-
-        builder.setPositiveButton("Open App Again", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-               recreate();
-            }
-        });
-
-        // Set the dialog to not be cancelable (user can't dismiss it)
-        builder.setCancelable(false);
-
-        // Create and show the dialog
-        AlertDialog dialog = builder.create();
-        dialog.show();
     }
+
     private void showBottomSheet() {
         BottomSheetCrash bottomSheetFragment = new BottomSheetCrash();
         bottomSheetFragment.show(getSupportFragmentManager(), bottomSheetFragment.getTag());
-    }
-    private void authenticateWithFingerprint() {
-        try {
-            KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
-            keyStore.load(null);
-
-            KeyGenerator keyGenerator = KeyGenerator.getInstance(
-                    KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
-
-            KeyGenParameterSpec.Builder builder = new KeyGenParameterSpec.Builder(
-                    KEY_NAME,
-                    KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
-                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
-                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7);
-
-            keyGenerator.init(builder.build());
-            SecretKey secretKey = keyGenerator.generateKey();
-
-            Cipher cipher = Cipher.getInstance(
-                    KeyProperties.KEY_ALGORITHM_AES + "/"
-                            + KeyProperties.BLOCK_MODE_CBC + "/"
-                            + KeyProperties.ENCRYPTION_PADDING_PKCS7);
-
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            FingerprintManagerCompat.CryptoObject cryptoObject =
-                    new FingerprintManagerCompat.CryptoObject(cipher);
-
-            fingerprintManager.authenticate(cryptoObject, 0, null,
-                    new FingerprintManagerCompat.AuthenticationCallback() {
-                        @Override
-                        public void onAuthenticationSucceeded(
-                                @NonNull FingerprintManagerCompat.AuthenticationResult result) {
-                            // Fingerprint authentication successful, navigate to the next screen
-                            Intent intent = new Intent(Crash.this, Landing.class);
-                            startActivity(intent);
-                            finish(); // Close the current activity
-                        }
-
-                        @Override
-                        public void onAuthenticationError(int errorCode, CharSequence errString) {
-                            // Handle authentication errors
-                        }
-                    }, null);
-        } catch (Exception e) {
-            e.printStackTrace();
-            // Handle exception
-        }
-    }
-    private void showPinCodeDialog() {
-        // Create a dialog to enter the pin code
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Enter PIN Code");
-
-        // Set an EditText for the user to enter the PIN
-        final EditText pinEditText = new EditText(this);
-        builder.setView(pinEditText);
-
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                String enteredPin = pinEditText.getText().toString();
-                if (enteredPin.equals("2908")) {
-                    // If the PIN matches, start another activity
-                    Intent intent = new Intent(Crash.this, Landing.class);
-                    startActivity(intent);
-                    finish(); // Close the current activity
-                } else {
-                    Toast.makeText(Crash.this, "Incorrect PIN", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                // Close the dialog
-            }
-        });
-        builder.setCancelable(false);
-        AlertDialog dialog = builder.create();
-        dialog.show();
     }
 }
